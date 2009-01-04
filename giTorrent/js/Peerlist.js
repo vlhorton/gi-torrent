7396205373d6094ebc921fa6441de2e77562b704
Hans.Hasert <Hans.Hasert@a0a57814-b44a-0410-b762-cd4aebd961e6>
2009-01-04 13:24:46 +0000

    Auto refresh on Peer list is always on.

--- giTorrent/js/Peerlist.js
+++ giTorrent/js/Peerlist.js
@@ -1,45 +1,38 @@
 jsx3.lang.Package.definePackage(
   "Peerlist.service",                //the full name of the package to create
   function(service) {          //name the argument of this function
-
     //call this method to begin the service call (Peerlist.service.call();)
     service.call = function() {
       var objService = giTorrent.loadResource("Peerlist_xml");
       objService.setOperation("");
-
       objGUI = giTorrent.getServer().getCache().clearById("Peer_list");
-      giTorrent.getJSXByName("matrix3").repaint();
-
       objService.setEndpointURL(giTorrent.getJSXByName("URI").getValue()+":"+giTorrent.getJSXByName("Port").getValue()+giTorrent.getJSXByName("Mount").getValue());
       objService.setUserName(giTorrent.getJSXByName("UserId").getValue());
       objService.setUserPass(giTorrent.getJSXByName("Password").getValue());
-
       //subscribe
       objService.subscribe(jsx3.net.Service.ON_SUCCESS, service.onSuccess);
       objService.subscribe(jsx3.net.Service.ON_ERROR, service.onError);
       objService.subscribe(jsx3.net.Service.ON_INVALID, service.onInvalid);
-
       //PERFORMANCE ENHANCEMENT: uncomment the following line of code to use XSLT to convert the server response to CDF (refer to the API docs for jsx3.net.Service.compile for implementation details)
       //objService.compile();
-
+      if ( giTorrent.getJSXByName("tabbedpane").getSelectedIndex() == 6 )
+      {
+         TimeoutID = window.setTimeout(Peerlist.service.call, giTorrent.getJSXByName("Refresh_Interval").getValue() * 1000);
+      } 
       //call the service
       objService.doCall();
     };
-
     service.onSuccess = function(objEvent) {
       //var responseXML = objEvent.target.getInboundDocument();
+      Speed.service.call();
       giTorrent.getJSXByName("matrix3").repaint(); 
     };
-
     service.onError = function(objEvent) {
       var myStatus = objEvent.target.getRequest().getStatus();
       objEvent.target.getServer().alert("Error","The service call failed. The HTTP Status code is: " + myStatus);
     };
-
     service.onInvalid = function(objEvent) {
       objEvent.target.getServer().alert("Invalid","The following message node just failed validation:\n\n" + objEvent.message);
     };
-
   }
 );
-
